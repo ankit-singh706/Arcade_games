cd8d75a221aa4ff4815eaa924071dcfa7a8cb6a7
ankit-cs706 <ankit8938@gmail.com>
2022-11-03 14:27:26 +0530

    Adding the tetris arcade machine

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Suspense, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+import {
+    Html,
+    Environment,
+    useGLTF,
+    ContactShadows,
+    OrbitControls,
+} from '@react-three/drei'
+import * as THREE from 'three'
+
+import './App.css'
+import Arcade from './Arcade'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <Canvas camera={{ position: [1, 200, 740]}}>
+            <Suspense fallback={null}>
+                <group position={[0, -200, -100]}>
+                    <Arcade />
+                </group>
+                <Environment preset='city' />
+            </Suspense>
+            <OrbitControls/>
+        </Canvas>
+    )
+}
+
+export default App
